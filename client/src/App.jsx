4fd2e8b95f4a34d80ffba84dc242bcea7d8e85d9
Narwey <benmbark.anouar@gmail.com>
2024-01-29 16:00:39 +0100

    Navbar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SingIn'
+import SignUp from './pages/SingUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <div>App</div>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path ="SignUp" element={<SignUp />} />
+          <Route path="SignIn" element={<SignIn/>} />
+        </Routes>
+      </BrowserRouter>
   )
 }
